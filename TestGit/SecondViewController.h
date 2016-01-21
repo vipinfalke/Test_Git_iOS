5f10016d43e34c750f9fb7815b82c6747c5fac15
GSS Admin <gssadmin@GSSs-Mac.local>
2016-01-21 17:36:26 +0530

    added second view controller

---
+++ TestGit/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  TestGit
+//
+//  Created by GSS Admin on 21/01/16.
+//  Copyright (c) 2016 GSS Infotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
