f7d9092ebf7245c84fd6d9c4453d0ec9ae7c33f5
GSS America Infotech <gssamericainfotech@gss-mac.local>
2016-01-21 17:46:12 +0530

    mac'comment

--- TestGit/SecondViewController.m
+++ TestGit/SecondViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //aafafaff
+    //asfaff
+    ///afsfaf
+    //added by mac
 }
 
 - (void)didReceiveMemoryWarning {
